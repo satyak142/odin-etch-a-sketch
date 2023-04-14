95b375061eff2ad45199e6913387a014b10a68b2
satya <120245070+satyak142@users.noreply.github.com>
2023-04-14 18:55:40 +0530

    grid expansion button added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 
 const sketchboard = document.querySelector('#sketchboard') ;
+const gridSize = document.querySelector('#gridSize') ;
+
 
 function gridCreation () {
-    for (let i = 0 ; i < 16*16 ; i++){
+    const size = gridSize.value;
+    gridSize.parentElement.firstElementChild.textContent = `Size(${size}x${size})`;
+    sketchboard.innerHTML = '';
+    for (let i = 0 ; i < size*size ; i++){
         const newDiv = document.createElement('div') ;
         sketchboard.appendChild(newDiv);
     }
+    sketchboard.style.setProperty('--size',size);
+    const allDivs = document.querySelectorAll('#sketchboard div') ;
+    allDivs.forEach(div => div.addEventListener('mouseover',draw));
 }
-gridCreation()
-const allDivs = document.querySelectorAll('#sketchboard div') ;
-allDivs.forEach(div => div.addEventListener('mouseover',draw));
+
+
 function draw(e){
     e.target.style.backgroundColor = '#000000'
 }
+
+gridCreation();
+gridSize.addEventListener('input',gridCreation)
+
+
+
 
